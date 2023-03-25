718704cb6f07eac3f54d7ad9aa2c31cd6c56e8c5
wttldt <williamthomaslevine@gmail.com>
2023-03-25 03:10:34 +0000

    Update 4 files

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,31 +4,28 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-3">
-        <Image
-          alt="header text"
-          src="/writingIcon.png"
-          className="sm:w-12 sm:h-12 w-8 h-8"
-          width={32}
-          height={32}
-        />
+      <Link href="/" className="flex items-center space-x-3">
+        <div className="flex items-center justify-center w-10 h-9 bg-black rounded-full">
+          <span className="text-3xl" style={{ marginTop: "5px" }}>
+            😒
+          </span>
+        </div>
         <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          twitterBio.com
+          g2tp.org
         </h1>
       </Link>
-      <a
-        href="https://vercel.com/templates/next.js/twitter-bio"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Image
-          alt="Vercel Icon"
-          src="/vercelLogo.png"
-          className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-          width={32}
-          height={28}
-        />
-      </a>
+      <div className="ml-auto">
+        <Link href="/" className="flex items-center space-x-3">
+          <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
+            Donate
+          </h1>
+          <div className="flex items-center justify-center w-10 h-9 bg-black rounded-full">
+            <span className="text-3xl" style={{ marginTop: "5px" }}>
+              😍
+            </span>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
